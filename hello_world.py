40a095ff3a27efd2f5dfd4fe83820dcae585f17b
AlexGrill1 <alexander.grill@posteo.de>
2022-06-05 17:43:17 +0200

    resolve merge conflict

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,4 @@
 print("Hello world")
 print("My first remote change")
 print("My second remote change")
-<<<<<<< HEAD
 print("Something different")
-=======
-print("My third remote change")
->>>>>>> 3381a2e1d789753d71a782dee548e01ac92a5a57
